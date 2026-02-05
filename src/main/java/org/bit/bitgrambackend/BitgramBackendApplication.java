6247b2918758c38a5a7528539cbd16646cdee839
sung9920 <sungcheol9920@gmail.com>
2026-02-05 17:07:05 +0900

    feat: Bitgram Init 설정

---
+++ src/main/java/org/bit/bitgrambackend/BitgramBackendApplication.java
@@ -0,0 +1,13 @@
+package org.bit.bitgrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitgramBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitgramBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bit/bitgrambackend/BitgramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bit.bitgrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitgramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
