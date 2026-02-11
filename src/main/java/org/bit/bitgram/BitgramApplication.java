f0515c428d90864bf4fe15f79b7804ed9986e85e
Yuna_H <ksm13563661@gmail.com>
2026-02-11 15:12:08 +0900

    feat(common): 공통 BaseEntity 구축 및 JPA Auditing 활성화
    - 도메인 엔티티의 시간 필드 중복 제거를 위해 CreatedTimeEntity, ModifiedTimeEntity 추가
    - 생성일 및 수정일 자동 주입 기능 활성화

--- src/main/java/org/bit/bitgram/BitgramApplication.java
+++ src/main/java/org/bit/bitgram/BitgramApplication.java
@@ -2,8 +2,10 @@ package org.bit.bitgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 자동 시간 기록 기능 활성화
 public class BitgramApplication {
 
 	public static void main(String[] args) {
